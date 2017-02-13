cba9ad372be6b866a3260d3de55cfff7ce55c9ca
Jan Bobisud <me@bobisjan.com>
2017-02-13 10:48:13 +0100

    Fetch GTM script in `HEAD` section

--- addon/metrics-adapters/google-tag-manager.js
+++ addon/metrics-adapters/google-tag-manager.js
@@ -31,16 +31,6 @@ export default BaseAdapter.extend({
     assert(`[ember-metrics] You must pass a valid \`id\` to the ${this.toString()} adapter`, id);
 
     set(this, 'dataLayer', dataLayer);
-
-    if (canUseDOM) {
-      /* jshint ignore:start */
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script',get(this, 'dataLayer'),id);
-      /* jshint ignore:end */
-    }
   },
 
   trackEvent(options = {}) {
--- index.js
+++ index.js
@@ -78,5 +78,32 @@ module.exports = {
         return regex.test(name) && whitelisted.indexOf(path.basename(name, '.js')) === -1;
       }]
     });
+  },
+
+  contentFor: function(type, config) {
+    var adapters = config.metricsAdapters || [];
+    var gtm = config.metricsAdapters.filter(item => item.name === 'GoogleTagManager')[0];
+
+    if (!gtm) {
+      return;
+    }
+
+    if (type === 'head') {
+      return `
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${gtm.config.id}');</script>
+        <!-- End Google Tag Manager -->
+      `;
+    }
+
+    if (type === 'body') {
+      return `
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${gtm.config.id}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      `;
+    }
   }
 };
