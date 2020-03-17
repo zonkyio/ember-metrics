ad863d12b989fc8ffee29fc02bcdf415dd135d69
Jan Bobisud <me@bobisjan.com>
2020-03-17 18:47:50 +0100

    Add autoboot option for GTM adapter

--- addon/metrics-adapters/google-tag-manager.js
+++ addon/metrics-adapters/google-tag-manager.js
@@ -19,7 +19,7 @@ export default BaseAdapter.extend({
 
   init() {
     const config = get(this, 'config');
-    const { id, envParams } = config;
+    const { id, envParams, autoboot } = config;
     const dataLayer = getWithDefault(config, 'dataLayer', 'dataLayer');
     const envParamsString = envParams ? `&${envParams}`: '';
 
@@ -27,19 +27,21 @@ export default BaseAdapter.extend({
 
     set(this, 'dataLayer', dataLayer);
 
-    (function(w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-        'gtm.start': new Date().getTime(),
-        event: 'gtm.js'
-      });
-      var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l !== 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl + envParamsString;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', get(this, 'dataLayer'), id);
+    if (autoboot !== false) {
+      (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+          'gtm.start': new Date().getTime(),
+          event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l !== 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl + envParamsString;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', get(this, 'dataLayer'), id);
+    }
   },
 
   trackEvent(options = {}) {
